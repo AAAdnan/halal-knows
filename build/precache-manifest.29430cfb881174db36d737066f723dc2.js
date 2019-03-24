752bd2db2631ee30a60c68dcbda6e5cc06a8555f
Adnan Ahmed <username@email.com>
2019-03-24 23:54:30 +0800

    deploy heroku

---
+++ build/precache-manifest.29430cfb881174db36d737066f723dc2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecbc32dd9ebad3825dfa",
+    "url": "/static/css/main.ace79d05.chunk.css"
+  },
+  {
+    "revision": "ecbc32dd9ebad3825dfa",
+    "url": "/static/js/main.f4a2a896.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "68210419042c99c5e854",
+    "url": "/static/css/2.a545f010.chunk.css"
+  },
+  {
+    "revision": "68210419042c99c5e854",
+    "url": "/static/js/2.cf1a176a.chunk.js"
+  },
+  {
+    "revision": "deab5e6b450ac900426778c80b0bc7a8",
+    "url": "/static/media/MosqueBackground.deab5e6b.jpg"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "623d1cec4f874e3e79286847d1aba722",
+    "url": "/index.html"
+  }
+];
